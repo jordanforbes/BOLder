998989d6620cee819dcd608c06ab73f73ed5d4ba
Jordan Forbes <jordforb@gmail.com>
2023-09-01 12:16:54 -0400

    added download link

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## to create executable:
 
 pyinstaller --onefile --name BOLder --noconsole main.py
+
+link: https://drive.google.com/file/d/1jnR1rWCr3gb1Bc7CwthzbLBKWK2mmyPw/view?usp=drive_link
--- gui.py
+++ gui.py
@@ -5,14 +5,14 @@ def gui(browse_input_file, browse_output_folder, convert):
   root = tk.Tk()
   root.title("BOLder")
 
-  browse_input_button = tk.Button(root, text="Browse", command=lambda: browse_input_file(entry_input_path))
+  browse_input_button = tk.Button(root, text="Select EPUB File", command=lambda: browse_input_file(entry_input_path))
   browse_input_button.pack()
 
   entry_input_path = tk.Entry(root, width=50)
   entry_input_path.pack()
 
 
-  browse_output_button = tk.Button(root, text = "Browse Output Folder", command=lambda: browse_output_folder(entry_output_folder))
+  browse_output_button = tk.Button(root, text = "Select Output Folder", command=lambda: browse_output_folder(entry_output_folder))
   browse_output_button.pack()
 
   entry_output_folder = tk.Entry(root, width=50)
